63d86d536905deb1ac44eeb0fa0ac6ae1cd1c67b
David Danzilio <ddanzilio@constantcontact.com>
2014-12-01 13:08:30 -0500

    This is requiring 'hiera_undef' but it should be requiring 'hiera_puppet'

--- lib/puppet/parser/functions/hiera_array_undef.rb
+++ lib/puppet/parser/functions/hiera_array_undef.rb
@@ -1,6 +1,6 @@
 module Puppet::Parser::Functions
   newfunction(:hiera_array_undef, :type => :rvalue) do |*args|
-    require 'hiera_undef'
+    require 'hiera_puppet'
     key, override = HieraPuppet.parse_args(args)
     HieraPuppet.lookup(key, :undef, self, override, :array)
   end
